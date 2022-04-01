366f2a3be46075499bc23ecae176df055e251f8a
Nazrawi Demeke <dnazrawi@gmail.com>
2022-04-01 14:04:58 +0300

    initial commit

---
+++ app/src/main/java/com/nazrawi/table/ui/TableActivity.kt
@@ -0,0 +1,12 @@
+package com.nazrawi.table.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nazrawi.table.R
+
+class TableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table)
+    }
+}
